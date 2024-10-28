efaebe68ad1fb17fdbf9fb9c1cae922338ce2120
manupadillaph <manuepad@gmail.com>
2024-10-28 20:47:05 -0300

    product opt and no opt

--- example/src/lib/SmartDB/FrontEnd/index.ts
+++ example/src/lib/SmartDB/FrontEnd/index.ts
@@ -1,7 +1,7 @@
 export * from './Dummy.FrontEnd.Api.Calls';
 export * from './Test.FrontEnd.Api.Calls';
 
-export * from './UserNoOpt.FrontEnd.Api.Calls';
-export * from './UserOpt.FrontEnd.Api.Calls';
+export * from './ProductNoOpt.FrontEnd.Api.Calls';
+export * from './ProductOpt.FrontEnd.Api.Calls';
 
 export * from './Free.FrontEnd.Api.Calls';
